4df3a4bde2858dde6cd9e5da9f337a91f2d6822e
GOMEZ Kevin <gomezkevin@hotmail.fr>
2018-08-01 16:17:29 +0200

    mise a jour 3.0

--- assets/js.js
+++ assets/js.js
@@ -1,14 +1,6 @@
-$(function () { 
-  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
-});  
-
-// $( window ).scroll(function() {   
- // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
-  $(".progress-bar").each(function(){
-    each_bar_width = $(this).attr('aria-valuenow');
-    $(this).width(each_bar_width + '%');
-  });
-       
- //  }  
-// });
-
+$(function() {
+  $('progress').each(function() {
+    var max = $(this).val();
+    $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+			});
+});
